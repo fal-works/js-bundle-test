cbb592f38967e995b99f3da2361bff88b6d3e30a
FAL <falworks.contact@gmail.com>
2021-01-01 16:01:21 +0900

    fix type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import p5 from "p5";
 import * as sketch from "./sketch";
 
-new p5((p) => {
+new p5((p: p5) => {
   p.setup = sketch.setup.bind(undefined, p);
   p.draw = sketch.draw.bind(undefined, p);
 });
