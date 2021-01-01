a704938bdfe9ff28eda4d7712d37b403f2c078c1
FAL <falworks.contact@gmail.com>
2021-01-01 17:53:57 +0900

    minor

--- scripts/terser.js
+++ scripts/terser.js
@@ -5,8 +5,8 @@ import { distFilePath, minFilePath } from "./config.js";
 const minify = async () => {
   const inputCode = await fs.promises.readFile(distFilePath);
 
-  const output = await terser.minify(inputCode.toString(), { ecma: 2015 });
-  if (!output.code) throw "terser failed.";
+  const output = await terser.minify(inputCode.toString());
+  if (!output.code) throw "terser failed to generate output code.";
 
   await fs.promises.writeFile(minFilePath, output.code);
 };
