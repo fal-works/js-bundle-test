69f283caa1525c0ebd10ce45acc7db69d7b1e534
FAL <falworks.contact@gmail.com>
2021-01-01 17:24:17 +0900

    do not use es2016+ in src

--- src/p5-util.ts
+++ src/p5-util.ts
@@ -6,7 +6,7 @@ export type P5Methods = Record<string, P5Method>;
 /** Creates a new `p5` instance with given methods assigned. */
 export const newP5 = (methods: P5Methods): p5 =>
   new p5((p) => {
-    for (const [methodName, methodBody] of Object.entries(methods)) {
-      p[methodName] = methodBody.bind(undefined, p);
+    for (const methodName of Object.keys(methods)) {
+      p[methodName] = methods[methodName].bind(undefined, p);
     }
   });
